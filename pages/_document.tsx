e2c6b5f6b034144b288b887e1ebd93180bcd9bc0
Carlos-kaspa <carloslimatwg@gmail.com>
2022-07-07 22:11:30 -0300

    init

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html>
-      <Head></Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
